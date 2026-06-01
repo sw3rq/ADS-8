6eb1bdef5cdbec393dfc4556fbd218aae1fc8472
sw3rq <layza2k13@gmail.com>
2026-06-02 01:24:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,8 +19,7 @@ void makeTree(BST<std::string>& tree, const char* filename) {
     while (file.get(ch)) {
         if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
             word += static_cast<char>(tolower(static_cast<unsigned char>(ch)));
-        }
-        else if (!word.empty()) {
+        } else if (!word.empty()) {
             tree.insert(word);
             wordCount++;
             word.clear();
