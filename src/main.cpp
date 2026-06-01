e43dc5aac9886ceab5880fe0215caa322b544df4
sw3rq <layza2k13@gmail.com>
2026-06-01 23:56:34 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 #include "bst.h"
 
 int main() {
-  BST<std::string> words;
-  makeTree(words, "src/war_peace.txt");
-  std::cout << "Tree height: " << words.depth() << "\n";
-  std::cout << "Search 'the': " << words.search("the") << "\n";
-  std::cout << "Search 'pierre': " << words.search("pierre") << "\n";
-  printFreq(words);
-  return 0;
+    BST<std::string> words;
+
+    makeTree(words, "C:/Users/Admin/source/repos/LabBST/x64/Debug/war_peace.txt");
+
+    std::cout << "Tree height: " << words.depth() << "\n";
+    std::cout << "Search 'the': " << words.search("the") << "\n";
+    std::cout << "Search 'pierre': " << words.search("pierre") << "\n";
+
+    printFreq(words);
+
+    std::cout << "\nDone! Check result/freq.txt\n";
+
+    return 0;
 }
